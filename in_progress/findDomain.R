55a59645929e4f58de66cae3d08b65d08c512af7
Rachel Queen <nrq2@newcastle.ac.uk>
2023-08-02 14:11:53 +0100

    Add new functions - in progress

---
+++ in_progress/findDomain.R
@@ -0,0 +1,205 @@
+#' Convert SP to SF object
+#'
+#' @param sp 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+SPToSF <- function(sp, sampleName){
+  ##convert sp to sf
+  sf <- sp::geometry(sp) %>% as("sf")
+  sf$sample_id <- sampleName
+  return(sf)
+ }
+
+#' Convert sf object to sp object
+#'
+#' @param sf 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+SFToSP <- function(sf){
+  sp <- sf %>% as("Spatial") %>% sp::geometry()
+}
+
+
+#' Add spot positions to sfe object
+#'
+#' @param sampleName 
+#' @param sfe 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' spotToSFE("sample03", sfe)
+#' 
+spotToSFE <- function(sfe, sampleName){
+  ## get spot positions
+  spots_include <- sfe$sample_id == sampleName
+  spots <- sfe[,spots_include] %>% 
+    spatialCoords() %>% 
+    data.frame() 
+  colnames(spots) <- c("Image_X", "Image_Y")
+  
+  ## use scale factors to determine pixel position
+  scaleFactor <- SpatialFeatureExperiment::imgData(sfe)[imgData(sfe)$sample_id 
+                                                        == sampleName, 
+                                                        "scaleFactor"]
+  spots$pixel_x <- spots$Image_X * scaleFactor
+  spots$pixel_y <- spots$Image_Y * scaleFactor
+  spot_sp <- spots  %>%
+    dplyr::select(pixel_x, pixel_y) %>%
+    as.matrix() %>%  
+    sp::SpatialPoints()
+  
+ 
+  ## add to sfe
+  annotGeometry(sfe, 
+                sample_id = sampleName, type = "spots") <- SPToSF(spot_sp, 
+                                                             sampleName)
+  
+  
+  return(sfe)
+}
+
+#' Add spots for all all sections
+#'
+#' @param sfe 
+#' @param sampleInfo 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+allSpots <- function(sfe, sampleInfo){
+  
+  for (sample_id in sampleInfo$sample_id){
+    sfe <- spotToSFE(sfe, sample_id)
+  }
+  return(sfe)
+}
+
+
+
+################################################################################
+#' Convert image to spatial polygon
+#'
+#' @param imgFile 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+imageToCoords <- function(imgFile, cln, fll){
+  ## domain coordinates
+  im <- imager::load.image(imgFile)
+  im <- imager::grayscale(im)
+  p0 <- plot(im)
+  
+  im <- imager::clean(im,cln) %>% imager::fill(fll)
+  plot(im)
+  hl <- imager::highlight(im)
+  
+  
+  coords <- lapply(hl, function(coord){data.frame(coord$x, coord$y)})
+  p1s <- lapply(coords, sp::Polygon)  %>% sp::Polygons(3)
+  domain_sp <- sp::SpatialPolygons(list(p1s))
+  return(domain_sp)
+  
+}
+
+
+#' Assign spots to histological domain
+#'
+#' @param domain_sp 
+#' @param spot_sp 
+#' @param sfe 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+domainToColData <- function(domain_sp, spot_sp, sfe, domain_name, sample_id){
+  domain_spots <- sp::over(spot_sp, domain_sp) %>% 
+    is.na() %>% 
+    ifelse(yes = "", no = domain_name)
+  
+  
+  ## add a new column to metadata, only if doesn't already exist
+  if(!domain_name %in% colnames(colData(sfe))){
+    colData(sfe)[, domain_name] <- ""
+  }
+  
+  colData(sfe)[sfe$sample_id == sample_id, domain_name] <-  domain_spots
+  
+  return(sfe)
+  
+}
+
+
+
+
+#' Title
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+test <- function(x){
+  return(y)
+}
+
+
+
+#' FindDomain
+#' 
+#' This function converts painted histological domains created in Image 
+#' processing software (eg Amira, or Photoshops) and converts them into 
+#' spatial polygons.
+#'  
+#'
+#' @param imgFile 
+#' @param cln 
+#' @param fll 
+#' @param domainName 
+#' @param sampleName 
+#' @param sfe 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' j <- 1
+#' domain_name <- annotations_df$domain[j]
+#' img_file <- file.path(annotationDir,annotations_df$jpg[j])
+#' sfe <- domainToSFE(img_file, domain_name, "C01", sfe)
+
+
+domainToSFE <-  function(imgFile, domainName, sampleName, sfe, cln = 3, fll = 12){
+ # convert spot coordinates back to sp
+  spot_sf <- annotGeometry(sfe, type = "spots", sample_id = sampleName)
+  spot_sp <- SFToSP(spot_sf)
+  
+  domain_sp <- imageToCoords(imgFile, cln, fll)
+  
+  
+  ## add to sfe geometries
+  annotGeometry(sfe,
+               sample_id = sampleName,
+               type = domainName) <- SPToSF(domain_sp, sampleName)
+  
+  ## add to coldata
+  sfe <- domainToColData(domain_sp, spot_sp, sfe, domain_name, sampleName)
+  
+  return(sfe)
+}
+
+
---
+++ in_progress/newPlotFunctions.R
@@ -0,0 +1,41 @@
+library(SpatialFeatureExperiment)
+
+
+sample_id = "C01"
+sfe2 <- readRDS("testData/rObjects/sfe.rds")
+sfe3 <- readRDS("testData/rObjects/sfe3.rds")
+
+
+### get spatraster
+sr <- getImg(sfe, sample_id)@image
+
+
+
+## pixel coordinates from sfe
+coords_sample <- spatialCoords(sfe[,sfe$sample_id == sample_id]) %>% 
+  data.frame()
+
+coords_sample$X <- coords_sample$pxl_col_in_fullres
+coords_sample$Y <- coords_sample$pxl_row_in_fullres
+coords_sample$Y <- coords_sample$Y * -1
+
+
+## test plot
+#df <- data.frame(X = 0:13636, Y = 0:13636  -13636)
+p1 <- ggplot(coords_sample) +
+  geom_spatraster_rgb(data = sr) + geom_point(aes(X, Y)) + scale_y_reverse()
+
+
+
+## rotate spatraster
+p2 <- p1 + scale_y_reverse()
+p2
+
+p2 + geom_point(aes(X, Y))
+
+## pixel coordinates from sfe
+coords_sample <- spatialCoords(sfe[,sfe$sample_id == sample_id])
+
+
+## scale pixels
+
