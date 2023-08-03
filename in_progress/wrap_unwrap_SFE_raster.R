123b4bdd42e9818183daf3e9cff25cfcaf69feb7
Rachel Queen <nrq2@newcastle.ac.uk>
2023-08-03 16:59:29 +0100

    wrap a single sfe object without crashing

--- in_progress/wrap_unwrap_SFE_raster.R
+++ in_progress/wrap_unwrap_SFE_raster.R
@@ -22,8 +22,10 @@ singleImg <- function(sample_id, sfe){
 #' @export
 #'
 #' @examples
+
 newSpatRasterImage <- function(x){
   new("SpatRasterImage",image = x)}
+
 
 ###############################################################################
 #' Wrap SpatRaster objects so that they can be reloaded
@@ -35,9 +37,16 @@ newSpatRasterImage <- function(x){
 #'
 #' @examples
 wrapSFE <- function(sfe){
+  if(length(imgData(sfe)$sample_id > 1)){
   SRI_list <- lapply(imgData(sfe)$sample_id, singleImg, sfe)
-  wrapped_SRI_list <- SRI_list %>% lapply(terra::wrap)
+  wrapped_SRI_list <- SRI_list %>% lapply(terra::wrap) #%>% lapply(PackedSpatRasterImage)
   sfe@int_metadata$imgData@listData$data <- wrapped_SRI_list
+  }
+  if(length(imgData(sfe)$sample == 1)){
+    sr <- SpatialExperiment::getImg(sfe)@image
+    wrapped_SRI <- terra::wrap(sr) #%>% new("PackedSpatRasterImage", image = .)
+    sfe@int_metadata$imgData@listData$data <- list(wrapped_SRI)
+   }
 return(sfe)
 }
 
