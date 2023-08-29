168d2d54cb503e3680c2591c761d9999a292ce2e
Rachel Queen <nrq2@newcastle.ac.uk>
2023-08-29 16:46:13 +0100

    changing to spanielPlot_sfe in last chunk

--- app_v2/shinyDash.R
+++ app_v2/shinyDash.R
@@ -1,3 +1,15 @@
+welcomeMessage <-  HTML(paste("Spaniel is an shiny app for sharing and exploring 
+        Spatial Transcriptomics data.",  "To get started click on the <b>'upload 
+        data'</b> option from the left-hand menubar.", sep = "</br>"))
+
+howToUpload <- HTML(paste("1) Click on <b>'Browse...'</b>" ,
+"2) Select a processed <b>SpatialFeatureExperiment</b> object 
+from your computer and click on <b>'Open'</b>",
+"3) Wait for the blue <b>'Upload to complete'</b> message to appear.",
+"4) You can now browse the dataset by clicking on the <b>Quality 
+Control</b> or <b>SpanielPlot</b> tabs", sep = "</br>"))
+
+
 header <- dashboardHeader(title = paste0("Spaniel X.0.0") )
 
 sidebar <- dashboardSidebar(
@@ -5,8 +17,9 @@ sidebar <- dashboardSidebar(
     menuItem("Main", tabName = "Main", icon = icon('home')),
     menuItem("Upload Object", tabName = "obj", icon = icon('upload')),
     menuItem("Quality control", tabName = "QC", icon = icon("dashboard")),
-    menuItem("Point Selection", tabName = "pselection", icon = icon("dna")),
-    menuItem("SpanielPlot", tabName = "spotplot", icon = icon('dog'))
+    #menuItem("Point Selection", tabName = "pselection", icon = icon("dna")),
+    menuItem("Gene Expression", tabName = "spotplot", icon = icon('dna')),
+    menuItem("Compare Sections", tabName = "spotplot", icon = icon('dog'))
   )
 )
 
@@ -14,17 +27,18 @@ dashBody <- dashboardBody(
   tabItems(
     tabItem(tabName = "Main",   fluidRow(
       box(
-        title = "Welcome to Spaniel!", status = "primary",
-        "A really cool introduction and a biorxiv link.", width = 12
+        title = "Welcome to Spaniel!", status = "primary", welcomeMessage, 
+        width = 12
       ))),
     tabItem(tabName = "obj", 
             fluidRow( 
               box(
-                title = "Upload SpatialFeatureExperiment object", status = 'primary',
+                title = "Upload SpatialFeatureExperiment object", 
+                status = 'primary',
                 fileInput('file1', 'Choose rds File',
                           accept=c('.rds'))),
               box(
-                title = "How to upload", "Instructions go here"
+                title = "How to upload", howToUpload
                 )
               )
             ),
--- app_v2/shinyModules.R
+++ app_v2/shinyModules.R
@@ -1,79 +1,89 @@
 spanielPlotUI <- function(id) {
-  fluidPage(
-  fluidRow(
+  fluidPage(fluidRow(
+    tabBox(
+      tabPanel(
+        "Input",
+        selectizeInput(
+          NS(id, "gene"),
+          "Genes",
+          choices = NULL,
+          selected = NULL,
+          multiple = FALSE
+        ),
+        selectizeInput(
+          NS(id, "sample"),
+          "Sample",
+          choices = NULL,
+          selected = NULL,
+          multiple = FALSE
+        ),
+        selectizeInput(
+          NS(id, "clustering"),
+          "Clustering method",
+          choices = NULL,
+          selected = NULL,
+          multiple = FALSE
+        )
+      ),
+      tabPanel("UMAP - Clusters", plotOutput(NS(id, "umapall")))
+    ),
     
-  tabBox(#width = NULL, 
-    #title = "input",
-    tabPanel("Input",
-      selectizeInput(NS(id, "gene"), "Genes", choices = NULL, selected = NULL, multiple = FALSE),
-      selectizeInput(NS(id, "sample"), "Sample", choices = NULL, selected = NULL, multiple = FALSE),
-      selectizeInput(NS(id, "clustering"), "Clustering method", choices = NULL, selected = NULL, multiple = FALSE)),
-    tabPanel("UMAP", plotOutput(NS(id, "umapall")))
-      
-  ),
-  tabBox(
-    tabPanel(
-      "Violin Plot",
-    plotOutput(NS(id, "vlnplot"))
-    ),
-    tabPanel(
-      "Spatial Plot",
-      plotOutput(NS(id, "plot"))
-    ),
-    tabPanel(
-      "UMAP",
-      plotOutput(NS(id, "umapplot"))
-    )
-  )
-  ),
-    fluidRow(
-  
-  box(
-    #width = NULL,
-    "plot",
+    box(title = "Spatial Plot",
+        plotOutput(NS(id, "plot")))
   ),
   
-  box(
-   # width = NULL,
-    "umap",
-  )
-)
-)
-
-  
+  fluidRow(
+    box(title = "Violin Plot",
+        plotOutput(NS(id, "vlnplot"))),
+    box(title = "UMAP Plot",
+        plotOutput(NS(id, "umapplot")))
+  ))
 }
 
+
 spanielPlotServer <- function(id, sfe) {
-  
   moduleServer(id, function(input, output, session) {
-    if(class(sfe) == "SpatialFeatureExperiment"){
+    if (class(sfe) == "SpatialFeatureExperiment") {
       observeEvent(c(input$gene, input$sample, input$clustering), {
-        if(input$gene == "" || input$sample == "" || input$clustering == ""){return()}
+        if (input$gene == "" ||
+            input$sample == "" || input$clustering == "") {
+          return()
+        }
         print(input$sample)
         sample_id <<- input$sample
         clust <<- input$clustering
-        
-        sr <- getImg(sfe, sample_id)@image
         sfeSample <- sfe[, sfe$sample_id == sample_id]
-        sf <- colGeometries(sfeSample)[[1]]
-        sf$clust <- sfeSample$clust
-        sf$barcodes <- rownames(sf)
-        counts_df <- as.data.frame(t(as.matrix(sfeSample@assays@data$logcounts)))
-        counts_df$barcodes <- rownames(counts_df)
-        sf <- sf %>% left_join(counts_df, by = 'barcodes')
-        
-        gene_name <<- input$gene
-        output$plot <-renderPlot({
-          ggplot(sf) +
-            geom_spatraster_rgb(data = sr) + geom_sf(data = sf, aes(fill = .data[[gene_name]] ) )
-        }, res = 96)
+        # sr <- getImg(sfe, sample_id)@image
+        # 
+        # sf <- colGeometries(sfeSample)[[1]]
+        # sf$clust <- sfeSample$clust
+        # sf$barcodes <- rownames(sf)
+        # counts_df <-
+        #   as.data.frame(t(as.matrix(
+        #     sfeSample@assays@data$logcounts
+        #   )))
+        # counts_df$barcodes <- rownames(counts_df)
+        # sf <- sf %>% left_join(counts_df, by = 'barcodes')
+        # 
+        # gene_name <<- input$gene
+        # output$plot <- renderPlot({
+        #   ggplot(sf) +
+        #     geom_spatraster_rgb(data = sr) + geom_sf(data = sf, aes(fill = .data[[gene_name]]))
+        # }, res = 96)
         
         output$umapplot <- renderPlot({
-          scater::plotUMAP(sfeSample, colour_by = gene_name, text_by = clust)
+          scater::plotUMAP(sfeSample,
+                           colour_by = gene_name,
+                           text_by = clust)
         })
         
         output$vlnplot <- renderPlot({
-          scater::plotExpression(sfeSample, features = gene_name, x = clust, colour_by = clust)
+          scater::plotExpression(
+            sfeSample,
+            features = gene_name,
+            x = clust,
+            colour_by = clust
+          )
         })
         
         output$umapall <- renderPlot({
@@ -82,34 +92,43 @@ spanielPlotServer <- function(id, sfe) {
         })
       })
     }
-    }
-    )
+  })
 }
 
 
 qcPlotUI <- function(id) {
-  fluidRow(
-      box(
-        #width = NULL,
-        "plot",
-        plotOutput(NS(id, "plot"))
+  fluidRow(box(#width = NULL,
+    "plot",
+    plotOutput(NS(id, "plot"))),
+    
+    box(
+      #width = NULL,
+      #title = "input",
+      selectizeInput(
+        NS(id, "metric1"),
+        "Metrics",
+        choices = NULL,
+        selected = NULL,
+        multiple = FALSE
+      ),
+      selectizeInput(
+        NS(id, "sample1"),
+        "Sample",
+        choices = NULL,
+        selected = NULL,
+        multiple = FALSE
       ),
       
-      box(#width = NULL, 
-        #title = "input",
-        selectizeInput(NS(id, "metric1"), "Metrics", choices = NULL, selected = NULL, multiple = FALSE),
-        selectizeInput(NS(id, "sample1"), "Sample", choices = NULL, selected = NULL, multiple = FALSE),
-        
-      )
-    )
+    ))
 }
 
 qcPlotServer <- function(id, sfe) {
-  
   moduleServer(id, function(input, output, session) {
-    if(class(sfe) == "SpatialFeatureExperiment"){
+    if (class(sfe) == "SpatialFeatureExperiment") {
       observeEvent(c(input$metric1, input$sample1), {
-        if(input$metric1 == "" || input$sample1 == ""){return()}
+        if (input$metric1 == "" || input$sample1 == "") {
+          return()
+        }
         print(input$sample)
         sample_id <<- input$sample1
         sr <- getImg(sfe, sample_id)@image
@@ -123,16 +142,15 @@ qcPlotServer <- function(id, sfe) {
         
         metric <<- input$metric1
         
-        output$plot <-renderPlot({
+        output$plot <- renderPlot({
           ggplot(sf) +
-            geom_spatraster_rgb(data = sr) + geom_sf(data = sf, aes(fill = .data[[metric]] ) )
+            geom_spatraster_rgb(data = sr) + geom_sf(data = sf, aes(fill = .data[[metric]]))
         }, res = 96)
         
-
+        
       })
     }
-  }
-  )
+  })
 }
 
 
